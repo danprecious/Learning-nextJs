63f0cd73533ba8fc3ea3b86b37406c4da3ef2989
Dan <danielkayode222@gmail.com>
2023-04-28 03:14:46 +0100

    got it right this time

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -11,3 +11,30 @@ const EventCategories = () =>{
 }
 
 export default EventCategories;
+
+
+
+export async function getStaticPaths() {
+
+    const {events} = await import ('/data/data.json');
+    const allPaths = events.map(ev =>{
+        return {
+            params: {
+                cat: ev.id.toString(),
+            }
+        }
+    })
+    console.log(allPaths);
+    return{
+        paths: allPaths,
+        fallback: false,
+    }
+}
+
+export async function getStaticProps(context){
+    const {events} = await import ('/data/data.json');
+    console.log(context);
+    return{
+        props: {},
+    }
+}
